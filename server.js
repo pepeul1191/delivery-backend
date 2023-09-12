0ae60eac657db15a13d76ac516cc07215faf75b4
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2023-09-11 23:02:50 -0500

    hola mundo

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const logger = require('morgan');
+const cors = require('cors');
+
+const app = express();
+app.use(logger());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(cors());
+app.disable('x-powered-by');
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+server.listen(port, 'localhost', () => {
+  console.log(`Aplicación NodeJS ${process.pid} Iniciada`)
+});
+
+app.get('/', (req, res) => {
+  res.send('Ruta raíz del backend');
+});
+
+app.get('/test', (req, res) => {
+  res.send('Esta es  la ruta TEST');
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send(err.stack);
+});
